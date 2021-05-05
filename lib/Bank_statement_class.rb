8e11fc92de1328572ad695248535240a5b40c5f5
David Greaves <dm_greaves@msn.com>
2021-05-05 10:29:57 +0100

    Created Bank_statement class and TDD initial account transaction history

---
+++ lib/Bank_statement_class.rb
@@ -0,0 +1,14 @@
+require_relative 'bank_tech_test'
+
+class Bank_statement
+
+  def initialize()
+    @transaction_history = []
+  end
+
+
+  def print_statement
+    @transaction_history
+  end
+
+end
--- lib/bank_tech_test.rb
+++ lib/bank_tech_test.rb
@@ -1,9 +1,12 @@
+require_relative 'Bank_statement_class'
+
 class Bank_account
 
   INITIAL_BALANCE = 0
 
-  def initialize(balance = INITIAL_BALANCE)
+  def initialize(balance = INITIAL_BALANCE, statement = Bank_statement.new)
     @balance = balance
+    @statement = statement
   end
 
   def current_balance
